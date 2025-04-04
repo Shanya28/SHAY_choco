cb31392aa119e1fe14dfa6d5349137b2e86f1178
shanya oniloude <shanya.oniloude@epitech.eu>
2025-04-04 10:48:18 +0100

    chocotest

---
+++ .github/workflows/my_strlen.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strlen
+** File description:
+** Counting character within a string
+*/
+
+#include <stdio.h>
+
+int my_strlen(char const *str)
+{
+    int x = 0;
+
+    while (str[x] > '\0') {
+    x++;
+    }
+    return (x);
+}
+
+int main()
+{
+    printf("%d\n", my_strlen("Annaelle"));
+}
